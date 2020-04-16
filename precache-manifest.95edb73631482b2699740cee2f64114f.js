831e7c470c76538f3438dd6ea74a3c0f1f570829
natali-gracia <natali1gracia@gmail.com>
2020-04-16 23:53:32 +0300

    Updates

---
+++ precache-manifest.95edb73631482b2699740cee2f64114f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc871e4e340519ca55bcbe6c79bd9e5",
+    "url": "/blog-on-react/index.html"
+  },
+  {
+    "revision": "e2b8a5c8f826cd83902d",
+    "url": "/blog-on-react/static/css/2.93a7d2f9.chunk.css"
+  },
+  {
+    "revision": "f17cfb8968e5a9262b0b",
+    "url": "/blog-on-react/static/css/main.7ac8bdef.chunk.css"
+  },
+  {
+    "revision": "e2b8a5c8f826cd83902d",
+    "url": "/blog-on-react/static/js/2.59a9d2b7.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/blog-on-react/static/js/2.59a9d2b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f17cfb8968e5a9262b0b",
+    "url": "/blog-on-react/static/js/main.b6854f94.chunk.js"
+  },
+  {
+    "revision": "52ca38fd298ab9449252",
+    "url": "/blog-on-react/static/js/runtime-main.1d390953.js"
+  },
+  {
+    "revision": "8e43477e0669bbb8b2486cb2a981173a",
+    "url": "/blog-on-react/static/media/fav-short (1).8e43477e.svg"
+  },
+  {
+    "revision": "aefdb0b3bd4951021d63225289fb8cb2",
+    "url": "/blog-on-react/static/media/fav-short (2).aefdb0b3.svg"
+  },
+  {
+    "revision": "5b670189b1fdbb49479a7d70a8e5307f",
+    "url": "/blog-on-react/static/media/insta-icon-gold.5b670189.png"
+  },
+  {
+    "revision": "4f77a7312d21f38f4ed7876d668fa876",
+    "url": "/blog-on-react/static/media/insta-icon-grey.4f77a731.png"
+  },
+  {
+    "revision": "0f769677524bba5f8edc325c6d0ee086",
+    "url": "/blog-on-react/static/media/insta-icon.0f769677.png"
+  },
+  {
+    "revision": "726b8fbc4d55092dae3c07c442aff5ad",
+    "url": "/blog-on-react/static/media/logo.726b8fbc.png"
+  },
+  {
+    "revision": "935f923c6c322b52db27857d26ff0c03",
+    "url": "/blog-on-react/static/media/pint-icon-gold.935f923c.png"
+  },
+  {
+    "revision": "fae376906127ca1a6f49bbfbb676ee62",
+    "url": "/blog-on-react/static/media/pint-icon-grey.fae37690.png"
+  },
+  {
+    "revision": "3d8cd23bb4dffc611776aaf256913046",
+    "url": "/blog-on-react/static/media/pint-icon.3d8cd23b.png"
+  },
+  {
+    "revision": "8276db7e016fe0db523a8bb8bf13e5fe",
+    "url": "/blog-on-react/static/media/quote-icon.8276db7e.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/blog-on-react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/blog-on-react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/blog-on-react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/blog-on-react/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "20856acb430a35c70e9436111a7d195b",
+    "url": "/blog-on-react/static/media/youtube-icon-gold.20856acb.png"
+  },
+  {
+    "revision": "da4b64209c7cae11fe6791ca9a9d970b",
+    "url": "/blog-on-react/static/media/youtube-icon-grey.da4b6420.png"
+  },
+  {
+    "revision": "62f278a28cc898e32086fb77f622df90",
+    "url": "/blog-on-react/static/media/youtube-icon.62f278a2.png"
+  }
+]);
